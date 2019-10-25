658057a09d17027743d86bdb4ce4acf099e8e9d4
mrlhz <mr__lhz@163.com>
2019-10-25 23:56:14 +0800

    mini vue

--- code/all.js
+++ code/all.js
@@ -1,6 +1,46 @@
+function isObject(obj) {
+  return !!obj && typeof obj === 'object'
+}
 
-const list = []
+class Vue {
+  constructor(options) {
+    this.data = options.data
+    this.observer(this.data)
+  }
 
-module.exports = {
-  list
+  observer(value) {
+    console.log(value, 'value');
+    if (!value || !isObject(value)) {
+      console.log('not object')
+      return
+    }
+
+    Object.keys(value).forEach((key) => {
+      console.log(key)
+      this.defineReactive(value, key, value[key])
+    })
+  }
+
+  defineReactive(obj, key, value) {
+    const self = this
+    Object.defineProperty(obj, key, {
+      get() {
+        return value
+      },
+      set(newValue) {
+        if (newValue === value) return
+        value = newValue
+        self.update(newValue)
+      }
+    })
+  }
+
+  update(value) {
+    cb(value)
+  }
+}
+
+function cb(value) {
+  console.log('update', value)
+  document.querySelector('#app').innerText = value
 }
