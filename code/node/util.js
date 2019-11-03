03e5560a2535fe8286cab26d23d1c640b70d1392
mrlhz <mr__lhz@163.com>
2019-11-03 23:23:42 +0800

    util promisify

---
+++ code/node/util.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+const util = require('util')
+const promisify = util.promisify
+
+// 
+{
+  const read = promisify(fs.readFile)
+
+  read(path.resolve(__dirname, '../data/name.txt'), 'utf8')
+    .then((data) => console.log(data))
+}
+
+{
+  const promisifyAll = (obj) => {
+    for (const key in obj) {
+      if (typeof obj[key] === 'function')
+        obj[key] = promisify(obj[key])
+    }
+  }
+
+  promisifyAll(fs)
+
+  fs.readFile(path.resolve(__dirname, '../data/name.txt'), 'utf8')
+    .then((data) => console.log(data))
+}
