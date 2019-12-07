77e4c7c587c647e90c1b62de3d7d4527c4d7c45f
mrlhz <mr__lhz@163.com>
2019-12-07 22:13:00 +0800

    rotate array

--- leetcode/189-rotate-array.js
+++ leetcode/189-rotate-array.js
@@ -10,12 +10,35 @@
 var rotate = function (nums, k) {
   const len = nums.length
   k = k % len
-
-  while (k >= 0) {
-    
+  while (k > 0) {
+    let stack = nums.pop()
+    nums.unshift(stack)
+    k--
   }
+  return nums
 }
 
 
-rotate([1, 2, 3, 4, 5, 6, 7], 3)
-// rotate([-1, -100, 3, 99], 2)
+// console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3))
+// console.log(rotate([-1, -100, 3, 99], 2))
+
+// 使用反转
+function _rotate(nums, k) {
+  const len = nums.length
+  k %= len
+  reverse(nums, 0, len - 1)
+  // nums.reverse()
+  reverse(nums, 0, k - 1)
+  reverse(nums, k, len - 1)
+  console.log(nums);
+}
+
+function reverse(array, start, end) {
+  while (start < end) {
+    [array[start++], array[end--]] = [array[end], array[start]]
+  }
+  return array
+}
+
+// reverse([1, 2, 3, 4, 5, 6, 7], 0, 6)
+_rotate([1, 2, 3, 4, 5, 6, 7], 3)
