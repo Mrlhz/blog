14f6a0660ea299994ca7b707620da970032d8c33
mrlhz <mr__lhz@163.com>
2020-02-05 23:57:26 +0800

    interview question

--- interview/js/base.js
+++ interview/js/base.js
@@ -361,3 +361,95 @@
   
   add = curring(add, 0)
 }
+
+
+{
+  Object.defineProperty(Array.prototype, 'reduce1', {
+    value: function(callback) {
+      if (this === null) {
+        throw new TypeError('Array.prototype.reduce called on null or undefined')
+      }
+
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function')
+      }
+
+      var o = Object(this)
+
+      var len = o.length
+
+      var k = 0
+      var value
+
+      if (arguments.length >=2) {
+        value = arguments[1]
+      } else {
+        while (k < len && !(k in o)) {
+          k++
+        }
+
+        if (k >= len) {
+          throw new TypeError('Reduce of empty array with no initial value')
+        }
+
+        value = o[k++]
+      }
+
+      while (k < len) {
+        if (k in o) {
+          value = callback(value, o[k], k, o)
+        }
+
+        k++
+      }
+
+      return value
+    }
+  })
+}
+
+
+{
+
+  function xhr(config) {
+    const { data = null, url, method = 'get', async = true } = config
+    const request = new XMLHttpRequest()
+
+    request.open(method.toUpperCase(), url, async)
+  
+    xhr.send(data)
+  }
+
+}
+
+
+{
+  var b = 1
+  function outer() {
+    var b = 2
+    function inner() {
+      b++
+      console.log('p1', b)
+      var b = 3
+      console.log('p2', b)
+    }
+    inner()
+  }
+
+  outer()
+}
+
+{
+  let a = {
+    n: 10
+  }
+
+  let b = a
+
+  b.m = b = {
+    n: 20
+  }
+
+  console.log(a)
+  console.log(b)
+}
--- interview/todo.md
+++ interview/todo.md
@@ -1,9 +1,48 @@
-css实现图片自适应宽高
 
+
+> css实现图片自适应宽高
 ```css
 .img-responsive {
   display: inline-block;
   height: auto;
   max-width: 100%;
 }
+```
+
+> 讲flex，手写出flex常用的属性，并且讲出作用
+
+```css
+.flex {
+  flex-wrap: wrap;
+  flex-direction: row;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: flex-start;
+  align-content: space-between;
+}
+```
+> BFC是什么
+
+```
+
+```
+项目里面的前端鉴权是怎么实现的？
+> vue里面的虚拟dom是怎么回事？
+https://juejin.im/post/5d3f3bf36fb9a06af824b3e2
+
+
+vue双向绑定讲一讲
+手写函数防抖和函数节流
+讲讲常用的es6语法，比如let、promise、class等等
+浏览器渲染过程，回流重绘等等，load、DOMContentLoaded等等事件的触发顺序
+从小米应用商店里面随便找了一个需求让我现场实现，写伪代码
+
+作者：前端亚古兽
+链接：https://juejin.im/post/5c356f68f265da61483bca61
+来源：掘金
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+> 
+
+```
 ```
