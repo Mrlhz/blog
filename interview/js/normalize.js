0d9adf1f58fd53bf2b41941db07d1fe9cdc077f5
mrlhz <mr__lhz@163.com>
2020-06-09 23:54:21 +0800

    normalize.js

---
+++ interview/js/normalize.js
@@ -0,0 +1,14 @@
+/**
+ * @description 实现一个 normalize 函数，能将输入的特定的字符串转化为特定的结构化数据
+ * @see https://github.com/Advanced-Frontend/Daily-Interview-Question/issues/331
+ * @example 
+ */
+
+
+
+// 示例一: 'abc' --> {value: 'abc'}
+// 示例二：'[abc[bcd[def]]]' --> {value: 'abc', children: {value: 'bcd', children: {value: 'def'}}}
+
+function normalize(str) {
+
+}
